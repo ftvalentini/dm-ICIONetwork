454dd5799706c6882a0dd085cfe10422d7d0e141
ftvalentini <ft.valentini@gmail.com>
2019-07-22 01:43:19 -0300

    get paths from communities (version FV)

---
+++ com_paths_FV.r
@@ -0,0 +1,61 @@
+source("libraries.r")
+source("functions.r")
+
+semilla = 1001
+
+# read data ------------------------------------------------------------
+
+mats = readRDS("data/working/matrices.rds")
+# create graphs 
+library(igraph)
+gs <- map(mats, mat_to_graph)
+
+
+# communities -----------------------------------------------------
+
+set.seed(semilla)
+# tibble con matrices y grafo y algoritmo_community
+dat_coms = tibble(matriz_name = names(gs)
+                  , matriz = mats
+                  , grafo = gs) %>% 
+  tidyr::crossing(metodo=c("infomap","lp")) %>% 
+  # identifica comms
+  mutate(coms = map2(.x=grafo, .y=metodo,
+                     function(x,y) get_coms(grafo=x,metodo=y)))
+
+# number of nodes of each community
+dat_coms %>% mutate(l = map_depth(coms, 2, length)) %>% 
+  unnest(l) %>% mutate( l = unlist(l))
+# DECISION -> TRABAJAMOS SOLO CON W1/W2 e INFOMAP
+
+# tibble con una fila por community
+dat = dat_coms %>%
+  dplyr::filter(matriz_name %in% c("mat_w1","mat_w2") & metodo=="infomap") %>% 
+  unnest(coms, .preserve=matriz) %>% 
+  mutate(com_id = row_number())
+
+
+# paths ----------------------------------------------------------------
+
+# install.packages("BiocManager")
+# BiocManager::install("NetPathMiner")
+library(NetPathMiner)
+
+# paths con mas de 2 paises para cada com_id cada minsize
+dat_out = dat %>%
+  tidyr::crossing(minsize = 5:10) %>% 
+  mutate(
+    paths = pmap(list(x=matriz, y=coms, z=minsize),
+                 function(x,y,z) heavy_paths(matriz=x, community=y, minsize=z))
+    )
+
+com_paths = dat_out %>% 
+  unnest(paths, .preserve=com_id)
+
+
+
+
+# save com - paths tibble ------------------------------------------------
+
+saveRDS(com_paths, "data/working/com_paths.rds")
+
--- functions.r
+++ functions.r
@@ -21,3 +21,45 @@ mat_to_graph <-  function(mat_g){
   return(g)
 }
 
+# extract communities from graph
+get_coms = function(grafo, metodo) {
+  
+  if (metodo=="infomap") com = cluster_infomap(grafo, modularity=F)
+  if (metodo=="lp") com = cluster_label_prop(grafo)
+  out = igraph::groups(com) %>%
+    # keep communities with more than 1 node
+    "["(map_lgl(., function(x) length(x)>1)) %>% 
+    # keep communities with more than 2 countries
+    "["(map_lgl(., function(x) length(unique(str_extract(x,"(.+)_"))) > 2))
+  
+  return(out)
+}
+
+
+# identifica heavy paths
+heavy_paths = function(matriz, community, minsize){
+  
+  # sectores de la comunidad
+  nodos = community %>% unlist(use.names=F) %>% sort()
+  primarios = nodos
+  # alternativa: primarios = nodos[str_detect(nodos, '_01T03|_05T06|_07T08|_09')]
+  
+  # NOTA: NO SE USA GRAFO CON SOLO LOS SECTORES DE LA COMUNIDAD
+  # SE USAN TODOS LOS EDGES DONDE ESTAN POR LO MENOS UNO DE ESOS SECTORES
+  # (si no crashea el pathranker)
+  
+  # crea grafo
+  adj_list = data.table::melt(matriz) %>%
+    setNames(c("from","to","weight")) %>%
+    dplyr::filter(from %in% nodos | to %in% nodos) %>%
+    dplyr::filter(weight > 0)
+  g = graph_from_data_frame(adj_list, directed=T)
+  
+  # paths mas probables del grafo -- se conservan solo si tiene mas de 2 paises
+  paths = pathRanker(g, method="prob.shortest.path",
+                     start=primarios, K=50, minPathSize=minsize)$paths %>% 
+    map("genes") %>% 
+    "["(map_lgl(., function(x) length(unique(str_extract(x,"(.+)_"))) > 2))
+  
+  return(paths)
+}
