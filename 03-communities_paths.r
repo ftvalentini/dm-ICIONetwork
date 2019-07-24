98afa223f1b55690900d6598e52f610c0beadb58
ftvalentini <ft.valentini@gmail.com>
2019-07-24 00:47:07 -0300

    update communties and paths scripts

--- 03-communities_paths.r
+++ 03-communities_paths.r
@@ -1,223 +1,60 @@
 source("libraries.r")
 source("functions.r")
 
+semilla = 1001
 
-# read matrices ------------------------------------------------------------
+# read data ------------------------------------------------------------
 
 mats = readRDS("data/working/matrices.rds")
+# create graphs and save
+library(igraph)
+gs <- map(mats, mat_to_graph)
+write_rds(gs, "data/working/grafos.rds")
 
+# communities -----------------------------------------------------
 
+set.seed(semilla)
+# tibble con matrices y grafo y algoritmo_community
+dat_coms = tibble(matriz_name = names(gs)
+                  , matriz = mats
+                  , grafo = gs) %>% 
+  tidyr::crossing(metodo=c("infomap","lp")) %>% 
+  # identifica comms
+  mutate(coms = map2(.x=grafo, .y=metodo,
+                     function(x,y) get_coms(grafo=x,metodo=y)))
 
+# number of nodes of each community
+dat_coms %>% mutate(l = map_depth(coms, 2, length)) %>% 
+  unnest(l) %>% mutate( l = unlist(l))
+# DECISION -> TRABAJAMOS SOLO CON W1/W2 e INFOMAP
 
-# looping graphs ----------------------------------------------------------
+# tibble con una fila por community
+dat = dat_coms %>%
+  dplyr::filter(matriz_name %in% c("mat_w1","mat_w2") & metodo=="infomap") %>% 
+  unnest(coms, .preserve=matriz) %>% 
+  mutate(com_id = row_number())
+# save communities
+saveRDS(dat %>% select(-matriz), "data/working/communities.rds")
 
-mat_to_graph <-  function(mat_g){
-  mat_to_use = mat_g
-  # adjacency list and vertices list
-  vertices = dimnames(mat_g)  %>% unlist() %>% unique()
-  adj_list = data.table::melt(mat_g) %>% 
-    setNames(c("from","to","weight")) %>% 
-    dplyr::filter(weight>0) 
-  # (drop weight if using UW?)
-  
-  # create graph
-  library(igraph)
-  g = graph_from_data_frame(adj_list, directed=T, vertices=vertices)
-  return(g)
-}
-
-
-# generating graphs 
-gs <- map(mats, mat_to_graph )
-
-# looping communities -----------------------------------------------------
-
-
-detCom <- function(g){ 
-  
-  c_im = cluster_infomap(g, modularity=F)
-  c_lp = cluster_label_prop(g)
-  
-  # keep communities with more than 1 node
-  grupos_im = igraph::groups(c_im) %>% "["(map_lgl(., function(x) length(x)>1))
-  grupos_lp = igraph::groups(c_lp) %>% "["(map_lgl(., function(x) length(x)>1))
-  
-  # keep communities with more than 1 country
-  gruposf_im = grupos_im %>%
-    "["(map_lgl(., function(x) length(unique(str_extract(x,"(.+)_"))) > 1))
-  gruposf_lp = grupos_lp %>%
-    "["(map_lgl(., function(x) length(unique(str_extract(x,"(.+)_"))) > 1))
-  
-  out <- list(gruposf_im = gruposf_im,
-              gruposf_lp = gruposf_lp)
-}
-
-# generating communites
-gs_com <- map(gs, detCom)
-
-# commnuity diagnostics --------------------------------------------------
-
-diagnostics1 <- function(gc){
-  q_com <- map_depth(gc,1,length)
-  num_country <- map_depth(gc, 2, .f = ~length(unique(str_sub(.,1,3))))
-  avg_country <- map_depth(num_country,1,~ mean(unlist(.)))
-  #return(q_com)
-  return(unlist(avg_country))
-  
-  
-}
-
-diagnostics2 <- function(gc){
-  q_com <- map_depth(gc,1,length)
-  return(unlist(q_com))
-  
-  
-}
-
-d1 <- map(gs_com, diagnostics1)
-d2 <- map(gs_com, diagnostics2)
-
-full_diagnostics <- function(diag1, diag2){
-  d1 <- bind_rows(diag1)
-  d2 <- bind_rows(diag2)
-  df <- rbind.data.frame(d1,d2)
-  df2 <- df %>% mutate(var = c("Avg_Country_im",
-                               "Avg_Country_lp",
-                               "N_communities_im",
-                               "N_communities_lp"))
-  
-}
-
-algorithm_diagnostics <- full_diagnostics(d1,d2)
-
-# rank paths ----------------------------------------------------------------
+# paths ----------------------------------------------------------------
 
 # install.packages("BiocManager")
 # BiocManager::install("NetPathMiner")
 library(NetPathMiner)
 
-# rank paths starting from primary sectors (but not from north america)
-path_network <- function(icoi_mat, community, matrices){
-  
-  nodos = community %>% unlist(use.names=F) %>% sort()
-  #del_nodos = vertices[!vertices %in% nodos]
-  
-  primarios = nodos[str_detect(nodos, pattern = paste(c('_01T03', 
-                                                        '_05T06',
-                                                        '_07T08'), collapse="|"))]
-  
-  primarios_sin_eeuu = primarios[str_detect(primarios, pattern = paste(c('USA', 
-                                                                         'CAN'), collapse="|"), negate = TRUE)]
-  matt = icoi_mat
-  matt[c(matrices[["mat_w1"]])<0.01] = 0
-  
-  adj_list2 = data.table::melt(matt) %>%
-    setNames(c("from","to","weight")) %>%
-    # dplyr::filter(str_detect(from,"ARG") | str_detect(to,"ARG")) %>%
-    dplyr::filter(from %in% nodos | to %in% nodos) %>%
-    dplyr::filter(weight > 0)
-  
-  g2 = graph_from_data_frame(adj_list2, directed=T)
-  #g2 = graph_from_data_frame(adj_list, directed=T)
-  
-  bb = 0
-  iter = 0
-  while (bb %>%
-         names() %>%
-         str_sub(., 1, 3) %>%
-         unique() %>%
-         length() <3) {
-  aa = pathRanker(g2, method="prob.shortest.path",start = primarios_sin_eeuu, K=1, minPathSize=7)
-  # paths = aa$paths %>% map("genes")
-  # keep = map_lgl(paths,
-  #                function(x) length(unique(str_extract(x,".+_")))>=3 & length(x)<9)
-  # paths[keep]
-  
-  bb = extractPathNetwork(aa, g2)
-  iter <- iter + 1
-  if (iter == 20) break
-}
-  return(bb)
-}
+# paths con mas de 2 paises para cada com_id cada minsize
+dat_out = dat %>%
+  tidyr::crossing(minsize = 5:10) %>% 
+  mutate(
+    paths = pmap(list(x=matriz, y=coms, z=minsize),
+                 function(x,y,z) heavy_paths(matriz=x, community=y, minsize=z))
+    )
+
+com_paths = dat_out %>% 
+  unnest(paths, .preserve=com_id) %>% 
+  left_join(dat %>% select(com_id, coms), by="com_id")
+
+# save paths with their communities
+saveRDS(com_paths, "data/working/com_paths.rds")
 
 
-# genrate rank for each community of each matrix
-path_in_community = vector(mode = "list")
-temp_res <- vector(mode = "list")
-path_in_mat <- vector(mode = "list")
-for (i in 1:length(mats)){
-  print(i)
-  for (j in 1:length(gs_com[[i]])){
-    print(j)
-    for (k in 1:length(gs_com[[i]][[j]])){
-    temp_res[[k]] = path_network(icoi_mat = mats[[i]],community = gs_com[[i]][[j]],matrices = mats )
-    
-    }
-  path_in_community[[j]] <-temp_res
-  }
-path_in_mat[[i]] <- path_in_community
-}
-
-
-
-# plot paths ----------------------------------------------------------------
-
-somePDFPath = "output/plots/ranks.pdf"
-pdf(file=somePDFPath)  
-
-
-mapping = data.frame(matrix = c("w1","w2","UW"), 
-                     algo1 = c("Infomap"),
-                     algo2 = c("LP"))
-
-for (i in 1:length(path_in_mat)){   
-  # par(mfrow = c(2,1))
-  print(i)
-  for (j in 1:length(path_in_mat[[i]])){
-    print(j)
-    for (k in 1:length(path_in_mat[[i]][[j]])){
-      print(k)
-      
-    if (ecount(path_in_mat[[i]][[j]][[k]]) != 0){  
-    lay = layout_with_fr(path_in_mat[[i]][[j]][[k]])
-    plot(path_in_mat[[i]][[j]][[k]], layout=lay,
-         edge.color="gray",
-         edge.arrow.size=.1,
-         edge.curved=0.1,
-         # vertex.label="",
-         vertex.label.dist=-0.8,
-         vertex.label.color="black",
-         vertex.label.cex=0.75,
-         vertex.size=4,
-         vertex.color="black",
-         main = paste(mapping[i,1], mapping[i,j+1], sep = " "))
-    }
-    # lay = layout_with_fr(path_in_community[[i]][[2]])
-    # plot(path_in_community[[i]][[2]], layout=lay,
-    #      edge.color="gray",
-    #      edge.arrow.size=.1,
-    #      edge.curved=0.1,
-    #      # vertex.label="",
-    #      vertex.label.dist=-0.8,
-    #      vertex.label.color="black",
-    #      vertex.label.cex=0.75,
-    #      vertex.size=4,
-    #      vertex.color="black")
-    }
-  }
-} 
-dev.off() 
-
-
-# plot full ------------------------------------------------------------------
-g <- gs[[2]]
-E(g)$edge.weights = E(g)$weight
-E(g)$edge.weights
-
-png("output/plots/prueba3.png", width=1600, height=1200)
-lay = layout_with_kk(g)
-plot(g, layout=lay, edge.color="white",
-     vertex.size=1,
-     vertex.label="",
-     vertex.color="black")
-dev.off()
---
+++ 04-plot_table_paths.r
@@ -0,0 +1,99 @@
+source("libraries.r")
+source("functions.r")
+
+semilla = 1001
+
+# read data ---------------------------------------------------------------
+
+gs = readRDS("data/working/grafos.rds")
+com_paths = read_rds("data/working/com_paths.rds")
+
+
+# plot and table per community --------------------------------------------
+
+paths_to_show = list()
+library(igraph)
+dev.off()
+# para cada matriz:
+for (m_name in c("mat_w1","mat_w2")) {
+  # graph
+  g = gs[[m_name]]
+  # subset table of results
+  tab = com_paths %>% 
+    dplyr::filter(matriz_name==m_name)
+  # ids of communities
+  com_ids = unique(tab$com_id)
+  # para cada comunidad:
+  for (i in seq_along(com_ids)) {
+    # get subgraph with all nodes from paths
+    all_paths = tab %>% dplyr::filter(com_id==com_ids[i]) %>% pull(paths)
+    nodes = all_paths %>% unlist() %>% unique()
+    subg = induced_subgraph(g, nodes)
+    # plot and save subgraph
+    set.seed(semilla)
+    lay = layout_with_fr(subg)
+    png("output/plots/"%+%m_name%+%"_"%+%com_ids[i]%+%".png", width=600, height=600)
+    par(mai=c(0.2,0.2,0.2,0.2))
+    plot(subg,
+         layout=lay,
+         edge.color="gray",
+         edge.arrow.size=.1,
+         edge.curved=0.1,
+         vertex.label.dist=-0.8,
+         vertex.label.color="black",
+         vertex.label.cex=0.9,
+         vertex.size=2,
+         vertex.color="black"
+    )
+    dev.off()
+    # table with one path per size and save (maxsize = 10)
+    paths_to_show[[m_name]][[as.character(com_ids[i])]] = 
+      tab %>%
+      dplyr::filter(com_id==com_ids[i]) %>% 
+      mutate(size = map_dbl(paths, length)) %>% 
+      group_by(size) %>%
+      slice(1) %>% ungroup() %>% dplyr::filter(size <= 10) %>% 
+      mutate(cgv = map_chr(paths, function(x) paste0(x, collapse=" -> "))) %>% 
+      select(size, cgv)
+    # saveRDS(paths_to_show, "output/paths"%+%m_name%+%"_"%+%com_ids[i]%+%".rds")
+  }
+}
+saveRDS(paths_to_show, "output/paths_to_show.rds")
+
+
+
+
+
+# # FBETTEO ----------------------------------------
+# 
+# w1_info <- com_paths %>%
+#   dplyr::filter(matriz_name == "mat_w1") 
+# 
+# clean_path <- vector(mode ="list")
+# 
+# for (i in 1:length(w1_info$paths)){
+#   clean_path[[i]] = unlist(w1_info$paths[[i]])
+#   q_country = map(clean_path,.f = ~length(unique(str_sub(.,1,3))))
+#   
+# }
+# 
+# path_string <- tibble(path_string = character())
+# 
+# for (i in 1:length(clean_path)){
+#   path_string[i,1] =   as.character(paste(clean_path[[i]], collapse = " "))
+# }
+# 
+# 
+# w1_info2 <- cbind.data.frame(w1_info, q_country = unlist(q_country)) %>%
+#   mutate(id = row_number()) %>%
+#   cbind.data.frame(., path_string) %>% 
+#   arrange(com_id, minsize, desc(q_country)) %>%
+#   group_by(com_id, minsize) %>%
+#   slice(1) %>%
+#   ungroup() %>%
+#   select(id, path_string) %>%
+#   distinct(path_string, .keep_all = TRUE)
+# 
+# # save table to output -------------------------------------------------
+# 
+# saveRDS(w1_info2, "output/w1_info2.rds")
